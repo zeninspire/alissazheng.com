ec3d002250bb50f80bfa0b294e1d6348d7f268c3
zeninspire <zeninspire@gmail.com>
2017-02-01 01:26:49 -0800

    flipclock added

--- client/components/Countdown.jsx
+++ client/components/Countdown.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Countdown from 'react-count-down';
+import moment from 'moment';
 
 class Count extends Component {
   render () {
-    let OPTIONS = { endDate: '02/07/2017 11:58 PM', prefix: 'til launch!' }
+    let OPTIONS = { endDate: '02/07/2017 11:58 PM', prefix: 'til launch!' };
  
     return (
-      <div>
+      <div className='Count'>
+
         <Countdown options={OPTIONS} />
+        <div className="yazclock"></div>
+      
       </div>
     )
   }
+
+  componentDidMount () {
+        
+    let start = moment();
+    let finish = moment([2017,1,7,23,58]); 
+    let diff = finish.diff(start, 'seconds')
+    $('.yazclock').FlipClock(diff, {
+		clockFace: 'DailyCounter',
+		countdown: true
+   })
+  }
+
 }
 
 export default Count
