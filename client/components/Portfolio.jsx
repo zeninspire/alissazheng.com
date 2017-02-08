662662e27af513a3471fe179c7892c208ead5442
zeninspire <zeninspire@gmail.com>
2017-02-08 00:56:34 -0800

    updated portfolio links

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -4,9 +4,9 @@ class Portfolio extends Component {
 	render () {
 		return (
 			<div>
-				<iframe src="http://www.local-spot.com" height="450" width="450">Loca-Spot</iframe>
-				<iframe src="https://bash-to-the-trivia.herokuapp.com" height="450" width="450">Loca-Spot</iframe>
-				<iframe src="https://bash-to-the-dating.herokuapp.com" height="450" width="450">Loca-Spot</iframe>
+				<iframe src="http://www.local-spot.com" height="540" width="960">Loca-Spot</iframe>
+				<iframe src="https://bash-to-the-trivia.herokuapp.com" height="540" width="960">Trivia</iframe>
+				<iframe src="https://bash-to-the-dating.herokuapp.com" height="540" width="960">Video Dating</iframe>
 			</div>
 		)
 	}
