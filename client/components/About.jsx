9380c5a92ce062c7171f676d5c0273802884c8c3
zeninspire <zeninspire@gmail.com>
2017-02-08 00:02:20 -0800

    updated about me

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -5,7 +5,13 @@ class About extends Component {
 	render () {
 		return (
 			<div>
-                You are in the About Me section of the website!
+				<img className="profilepic" src="./img/profile.jpg" />
+				<ul>
+				Full-stack Software Engineer. <br/>
+				Solves problems from a user's perspective. <br/>
+				Builds web apps using JavaScript, Node.js, Express.js. <br/>
+				Delivers seamless user experience via React/Redux, Angular, Vue.js, etc. <br/>
+				</ul>
 				<Links />
 		        {this.props.children}
 			</div>
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Count from './Countdown.jsx';
-import Links from './Links.jsx';
 import Navigation from './Navigation.jsx'
 
 class App extends Component {
 	render () {
 		return (
 			<div>
-				Welcome to Alissa's Portfolio!<br/>
 				<Navigation />
+				Welcome to Alissa's Portfolio!<br/>
 		        {this.props.children}
 			</div>
 		)
