0dd2e18028a7c509b40ea017dcfe9a2b356defc2
zeninspire <zeninspire@gmail.com>
2017-04-17 19:21:53 -0700

    updated about section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -7,9 +7,11 @@ class About extends Component {
 		return (
 			<div className="about col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<ul className="AboutContent">
-						<img className="profilepic" src="./img/profile.jpg" /><br/>
+						<img className="profilepic" src="./img/profile.jpg" />
 						<Button bsStyle='info' onClick = {() => window.open('https://drive.google.com/open?id=0Bx8IDkDn2v9XLXZNVG5CT0lYbXc', '_blank')}>Resume</Button><br/>
-						Full-stack Software Engineer. <br/>
+						
+						<h3>Software Engineer | Full Stack </h3><br/>
+						<h4>React/Redux, Angular, Node.js, Python</h4><br/>
 						Solves problems from a user's perspective. <br/>
 						Builds web apps using JavaScript, Node.js, Express.js. <br/>
 						Delivers seamless user experience via React/Redux, Angular, Vue.js, etc. <br/>
