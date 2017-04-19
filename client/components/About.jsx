25fbcc1c178d8a212dc6cc85ecd4d47cf35b871d
zeninspire <zeninspire@gmail.com>
2017-04-18 22:13:35 -0700

    background added

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -7,14 +7,19 @@ class About extends Component {
 		return (
 			<div className="about col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<ul className="AboutContent">
-						<img className="profilepic" src="./img/profile.jpg" />
+						<img className="profilepic" src="./img/profile.jpg" />&nbsp;&nbsp;&nbsp;
 						<Button bsStyle='info' onClick = {() => window.open('https://drive.google.com/open?id=0Bx8IDkDn2v9XLXZNVG5CT0lYbXc', '_blank')}>Resume</Button><br/>
 						
-						<h3>Software Engineer | Full Stack </h3><br/>
-						<h4>React/Redux, Angular, Node.js, Python</h4><br/>
-						Solves problems from a user's perspective. <br/>
-						Builds web apps using JavaScript, Node.js, Express.js. <br/>
-						Delivers seamless user experience via React/Redux, Angular, Vue.js, etc. <br/>
+						<div className='about-text'>
+							<h3>Software Engineer | Full Stack </h3><br/>
+							<h4>React/Redux, Angular, Node.js, Python</h4><br/>
+						</div>
+							<h4>
+							Solves problems from a user's perspective. <br/>
+							Builds web apps using JavaScript, Node.js, Express.js. <br/>
+							Delivers seamless user experience via React/Redux, Angular, Vue.js, etc. <br/>
+							</h4>
+
 					</ul>
 		        {this.props.children}
 			</div>
